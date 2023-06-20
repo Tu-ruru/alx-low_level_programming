43b43286c0fd66219be5c572ccbcafee3d77abd7
Tu-ruru <ferh2592@gmail.com>
2023-06-19 18:16:38 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /**
-*main - Entry point 
+*main - Entry point
 *
 *Return:Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
-    puts("\"Programming is like building a mulitilingual puzzle");
-     return (0); 
+puts("\"Programming is like building a mulitilingual puzzle");
+return (0);
 }
